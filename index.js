2aff8da92571fb9f565724d8c69a95f524f5faa4
Sheuli Akter Shathy <sheuliakter85@gmail.com>
2023-01-22 02:03:36 +0600

    revise

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config();
-
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -13,82 +12,113 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7czv3fq.mongodb.net/?retryWrites=true&w=majority`;
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-async function run(){
-    try{
-        const appointmentOptionCollection=client.db('healthcare').collection('appointmentOptions');
-        const bookingsCollection=client.db('healthcare').collection('bookings');
+async function run() {
+  try {
+    const appointmentOptionCollection = client
+      .db("healthcare")
+      .collection("appointmentOptions");
+    const bookingsCollection = client.db("healthcare").collection("bookings");
+    const usersCollection = client.db("healthcare").collection("users");
 
-        const usersCollection = client.db("healthcare").collection("users");
-        
-    
-        
-        app.post("/users", async (req, res) => {
-          const user = req.body;
-          const result = await usersCollection.insertOne(user);
-          res.send(result);
-        });
+    // post
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
 
-        app.get('/appointmentOptions', async(req,res) =>{
-            const query = {};
-            const options = await appointmentOptionCollection.find(query).toArray();
-            res.send(options);
-          })
+    app.post("/bookings", async (req, res) => {
+      const booking = req.body;
+      const query = {
+        appointmentDate: booking.appointmentDate,
+        email: booking.email,
+        treatment: booking.treatment,
+      };
+      const alreadyBooked = await bookingsCollection.find(query).toArray();
 
-          app.post('/bookings', async(req,res) =>{
-            const booking = req.body;
-            const query = {
-             appointmentDate: booking.appointmentDate,
-             email: booking.email,
-             treatment: booking.treatment
-            }
-            const alreadyBooked = await bookingsCollection.find(query).toArray();
-    
-            if(alreadyBooked.length){
-                const message = `You already have a booking on ${booking.appointmentDate}. Choose new date.`;
-                return res.send({acknowledged:false, message})
-            }
-            const result = await bookingsCollection.insertOne(booking);
-            res.send(result);
-           });
-   
-           app.get('/bookings', async(req,res) =>{
-            const email = req.query.email;
-            const query = { email: email};
-            const bookings = await bookingsCollection.find(query).toArray();
-            res.send(bookings)
-          });
+      if (alreadyBooked.length) {
+        const message = `You already have a booking on ${booking.appointmentDate}. Choose new date.`;
+        return res.send({ acknowledged: false, message });
+      }
+      const result = await bookingsCollection.insertOne(booking);
+      res.send(result);
+    });
 
+    // get
+    app.get("/appointmentOptions", async (req, res) => {
+      const query = {};
+      const options = await appointmentOptionCollection.find(query).toArray();
+      res.send(options);
+    });
 
-          app.get('/users/admin/:email', async(req,res) =>{
-            const email = req.params.email;
-            const query = { email };
-            const user = await usersCollection.findOne(query);
-            res.send({isAdmin: user?.role === 'admin'});
-          })
-        //    app.get('/bookings/:email',(req,res) =>{
-        //     console.log(req)
-        //     const email = req.params.email;
-        //     console.log(email)
-        //     const bookings = bookingsCollection.find(booking=>booking.email===email)
-        //     res.send(bookings)
-        //   });
+    app.get("/bookings", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const bookings = await bookingsCollection.find(query).toArray();
+      res.send(bookings);
+    });
 
-        //   app.get('/checkouts/:id', (req, res) => {
-        //     const id = req.params.id;
-        //     const checkoutCourse = checkouts.find(c => c.id === id);
-        //      res.send(checkoutCourse);
-        //  });
-    }
-    finally{
+    app.get("/users/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const user = await usersCollection.findOne(query);
+      res.send({ isAdmin: user?.role === "admin" });
+    });
 
-    }
+    app.get("/allPatients", async (req, res) => {
+      const query = {};
+      const allPatients = await bookingsCollection.find(query).toArray();
+      res.send(allPatients);
+    });
+
+    app.get("/dashboard/updatedBooking/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const updatedBooking = await bookingsCollection.findOne(query);
+      res.send(updatedBooking);
+    });
+
+    // patch
+    app.patch("/update/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const update = req.body;
+      const option = { upsert: true };
+      const updateBooking = {
+        $set: {
+          drname: update.drname,
+          phone: update.phone,
+          message: update.message,
+        },
+      };
+      const result = await bookingsCollection.updateOne(
+        filter,
+        updateBooking,
+        option
+      );
+      res.send(result);
+    });
+
+    //   delete
+    app.delete("/patient/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await bookingsCollection.deleteOne(filter);
+      res.send(result);
+    });
+  } finally {
+  }
 }
-run().catch(console.log)
+run().catch(console.log);
 
-app.get('/', async(req,res) =>{
-    res.send('HealthCare is running on server')
-   })
-   
-app.listen(port, ()=> console.log(`Healthcare is running on: ${port}`))
+app.get("/", async (req, res) => {
+  res.send("HealthCare is running on server");
+});
+
+app.listen(port, () => console.log(`Healthcare is running on: ${port}`));
